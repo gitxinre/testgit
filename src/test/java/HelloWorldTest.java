7efd985ec3c087f0422682032e89cb85f5883eb3
mfl1249 <mfanlong@sina.com>
2018-09-13 11:47:30 +0800

    second commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Administrator
+ * @date 2018/9/13 11:40
+ */
+public class HelloWorldTest {
+
+
+    @Test
+    public void testSayHello() {
+        System.out.println("testSayHello!");
+    }
+
+
+}
