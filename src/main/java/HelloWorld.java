4635389a248702ca2d40ebe77db15c625ce97dc2
mfl1249 <mfanlong@sina.com>
2018-09-13 11:31:22 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Administrator
+ * @date 2018/9/13 11:25
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+}
